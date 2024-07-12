3b4075bbfef4a0e0824014b10aaea3451bf35fbd
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 11:29:17 -0400

    function test

---
+++ fuctions.sh
@@ -0,0 +1,24 @@
+# function is a name given to a set of commands
+# syntax
+# function abc() {
+# commands
+# commands
+# }
+
+# or
+# abc() {
+# commands
+# commands
+#}
+
+# How to call a function - simply call a function like a command
+
+# example
+
+SAMPLE() {
+  echo sample Function
+}
+
+SAMPLE
+SAMPlE
+SAMPLE
