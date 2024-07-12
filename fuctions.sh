667ea561ba251980f920cbc9378ac2f03ec4c86f
Naren <alekhyabonkuri96@gmail.com>
2024-07-12 11:50:44 -0400

    nodejs changes

--- fuctions.sh
+++ fuctions.sh
@@ -1,17 +1,21 @@
 # function is a name given to a set of commands
 # syntax
+
 # function abc() {
 # commands
 # commands
 # }
 
 # or
+
 # abc() {
 # commands
 # commands
 #}
 
 # How to call a function - simply call a function like a command
+
+# abc
 
 # example
 
