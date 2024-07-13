cf4be97f6c20ec8dfa441d8dd27848b640230105
Naren <alekhyabonkuri96@gmail.com>
2024-07-13 00:36:35 -0400

    shell script

--- functions.sh
+++ functions.sh
@@ -40,3 +40,15 @@ DEMO abc
 # variable names should be using alphabets and nubmers and _(underscore)
 #if values are having any special characters then double quote it.
 
+# The below function helps to know the status of every command in form of success or failure.
+#STAT() {
+# if [ $1 -eq 0 ]; then
+#    echo -e "\e[32mSUCCESS\e[0m"
+#  else
+#   echo -e "\e[31mFAILURE\e[0m"
+#    echo
+#   echo "Refer the log file for more information : File path : ${LOG_FILE}"
+#    exit $1
+# fi
+# }
+
